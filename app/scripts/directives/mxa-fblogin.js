083d8ac950bf763d425eb178de7402fa087a1bb6
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-01-11 19:32:51 +0900

    facebook directive is updated to use the latest format and increase the icon size

--- app/scripts/directives/mxa-fblogin.js
+++ app/scripts/directives/mxa-fblogin.js
@@ -10,7 +10,7 @@
 angular.module('directiveTestApp')
   .directive('mxaFblogin', function ($window, UserAuthService) {
     return {
-      template: '<fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>',
+      template: '<div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" scope="public_profile,email" onlogin="checkLoginState();"></div>',
       restrict: 'E',
       link: function(scope, element, attrs, controller) {
 
@@ -20,7 +20,7 @@ angular.module('directiveTestApp')
 			      cookie     : true,  
 			                          // the session
 			      xfbml      : true,  // parse social plugins on this page
-			      version    : 'v2.1' // use version 2.1
+			      version    : 'v2.5' // use version 2.1
 			    });
 			    FB.getLoginStatus(function(response) {
 			    //  statusChangeCallback(response);
@@ -28,10 +28,10 @@ angular.module('directiveTestApp')
 			  };
 			  // Load the SDK asynchronously
 			  (function(d, s, id) {
-			    var js, fjs = d.getElementsByTagName(s)[0];
-			    if (d.getElementById(id)) return;
-			    js = d.createElement(s); js.id = id;
-			    js.src = "//connect.facebook.net/en_US/sdk.js";
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+			    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=817436101604719";
 			    fjs.parentNode.insertBefore(js, fjs);
 			  }(document, 'script', 'facebook-jssdk'));
 				
