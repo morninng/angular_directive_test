44d2b12d0ae88ebebfd176a05a2df078256cb6a3
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-01-22 20:04:38 +0900

    event date time can be changed for filter, it appears by mouseover and dissapear by mouse leave

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.bootstrap'
+    'ui.bootstrap',
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/eventlist.js
+++ app/scripts/controllers/eventlist.js
@@ -11,6 +11,20 @@ angular.module('directiveTestApp')
   .controller('EventlistCtrl', function ($scope, $uibModal,EventListService) {
 
     $scope.event_list = EventListService.event_list;
+    $scope.date_range_show = false;
+    $scope.date_from = new Date();
+    $scope.date_to = new Date();
+    $scope.date_to.setMonth( $scope.date_to.getMonth() + 13 );
+    $scope.format = 'yyyy/MM/dd';
+
+
+    $scope.time_from = new Date();
+    $scope.time_from.setHours(0);
+    $scope.time_from.setMinutes(0);
+    $scope.time_to = new Date();
+    $scope.time_to.setHours(23); 
+    $scope.time_to.setMinutes(59); 
+
 
   	$scope.open_create_event = function(){
   		console.log(" open create event was called");
@@ -21,6 +35,29 @@ angular.module('directiveTestApp')
         size:'lg'
 		  })
   	}
+
+    $scope.date_range_in = function(){
+      if(!$scope.date_range_show ){
+        $scope.date_range_show = true;
+      }
+    }
+
+    $scope.data_range_out = function(){
+      if($scope.date_range_show ){
+        $scope.date_range_show = false;
+      }
+    }
+
+    $scope.time_range_in = function(){
+      if(!$scope.time_range_show ){
+        $scope.time_range_show = true;
+      }
+    }
+    $scope.time_range_out = function(){
+      if($scope.time_range_show ){
+        $scope.time_range_show = false;
+      }
+    }
 
 
 
